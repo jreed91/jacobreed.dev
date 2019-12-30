fbfa382f3a4809d4e8d89a8f6f30bb7e59c611dd
Jacob Reed <jreed91@gmail.com>
2019-12-29 20:43:08 -0600

    Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Jacob Reed | A Stream-of-Consciousness',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A website dedicated to tech, photos, music and travel',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
