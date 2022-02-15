725ac33ab3870ca38eeb28d320b2141ac9ea204c
Jacob Reed <jreed91@gmail.com>
2022-02-15 15:29:59 -0600

    Remove unnecessary type

--- pages/api/projects/handler.tsx
+++ pages/api/projects/handler.tsx
@@ -2,10 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma';
 
-type Data = {
-  name: string
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
