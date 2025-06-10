878f7dafa099c8cc99d021a93db71a24056e1905
Jacob Reed <jreed91@gmail.com>
2025-06-09 19:13:55 -0500

    Refine layout and align blog entries

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -6,16 +6,15 @@ export default function BlogPost ({ blog }: { blog: Blog}) {
   return (
     <Link href={`/blog/${blog.slug}`} className="w-full">
         <div className="w-full mb-8 transform hover:scale-[1.01] transition-all rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 p-4">
-          <div className="flex flex-col justify-between md:flex-row">
-            <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+          <div className="flex items-center justify-between w-full">
+            <h4 className="text-lg md:text-xl font-medium text-gray-900 dark:text-gray-100">
               {blog.metadata.title}
             </h4>
-            <div className="">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              className="h-6 w-6 ml-1"
+              className="h-5 w-5 ml-2 shrink-0"
             >
               <path
                 stroke="currentColor"
@@ -25,9 +24,8 @@ export default function BlogPost ({ blog }: { blog: Blog}) {
                 d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
               />
             </svg>
-            </div>
           </div>
-          <p className="text-gray-600 dark:text-gray-400">{blog.metadata.summary}</p>
+          <p className="mt-1 text-gray-600 dark:text-gray-400">{blog.metadata.summary}</p>
         </div>
     </Link>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,33 +18,31 @@ export default function Projects() {
   const projects = data;
   
   return (
-      <div className="items-center justify-between w-full relative max-w-4xl mx-auto px-8">
+      <div className="flex flex-col space-y-6 w-full relative max-w-4xl mx-auto px-8">
       <h3 className="mt-8 mb-4 text-2xl font-bold tracking-tight text-black md:text-4xl dark:text-white">
           All Projects
         </h3>
         {projects?.map((project) => (
           <Link href={`${project.slug}`} key={project.slug} className="w-full">
               <div className="w-full mb-8 transform hover:scale-[1.01] transition-all rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 p-4">
-                <div className="flex flex-col justify-between md:flex-row">
-                  <h4 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
+                <div className="flex items-center justify-between w-full">
+                  <h4 className="text-lg md:text-xl font-medium text-gray-900 dark:text-gray-100">
                     {project.name}
                   </h4>
-                  <div className="">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="h-6 w-6 ml-1"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-                      />
-                    </svg>
-                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="h-5 w-5 ml-2 shrink-0"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                    />
+                  </svg>
                 </div>
                 <p className="text-gray-600 dark:text-gray-400">
                   {project.description}
